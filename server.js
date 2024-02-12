cf70f649ab943c71b524e5595a2859d76389d130
jose rodriguez <jose.rodriguez.gomez.1992@gmail.com>
2024-02-12 00:54:22 -0300

    v2

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+// Sirve los archivos estáticos desde la carpeta dist
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+// Maneja las rutas del lado del cliente, devolviendo index.html
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 const port = process.env.PORT || 80;
 app.listen(port);
 
-console.log(`La app está escuchando en el puerto: ${port}`);
+console.log('La app está escuchando en el puerto: ' + port);
+
